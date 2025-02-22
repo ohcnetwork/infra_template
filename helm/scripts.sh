5f5c1aefd634a7e6e664d5fccaebd23cf6058fba
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2025-02-22 12:18:46 +0300

    update yaml files

--- helm/scripts.sh
+++ helm/scripts.sh
@@ -1,14 +1,16 @@
-
+# Install or upgrade the ingress-nginx controller
 helm upgrade --install ingress-nginx ingress-nginx \
- --repo https://kubernetes.github.io/ingress-nginx \
- --namespace ingress-nginx --create-namespace \
- --set controller.service.loadBalancerIP=34.100.159.155
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace \
+  --set controller.service.loadBalancerIP=34.100.159.155
 
+# Add the Jetstack repository for cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install \
-  cert-manager jetstack/cert-manager \
+
+# Install or upgrade cert-manager with the specified version
+helm upgrade --install cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.8.0 \
+  --version v1.12.0 \
   --set installCRDs=true
